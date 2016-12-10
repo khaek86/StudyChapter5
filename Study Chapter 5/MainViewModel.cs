ea14c9f086d74d761f7fa56c125f00a8715a281b
khaek86 <bkeunho86@gmail.com>
2016-12-10 11:58:04 +0900

    과외 하면서 추가한 것들 commit

---
+++ Study Chapter 5/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Chapter_5
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> ListBoxItems { set; get; }
+
+        public MainViewModel()
+        {
+            ListBoxItems = new ObservableCollection<string>();
+        }
+
+        private bool checkBoxChecked;
+        public bool CheckBoxChecked
+        {
+            set
+            {
+                checkBoxChecked = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("CheckBoxChecked"));
+                }
+            }
+            get
+            {
+                Debug.WriteLine("MainViewModel.CheckBoxChecked: " + checkBoxChecked);
+                //ListBoxItems.Add("Auto!!!");
+                return checkBoxChecked;
+            }
+        }
+
+        private string targetText;
+        public string TargetText
+        {
+            set
+            {
+                targetText = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("TargetText"));
+                }
+            }
+            get
+            {
+                Debug.WriteLine("MainViewModel.TargetText: " + targetText);
+                return targetText;
+            }
+        }
+
+        private void MyBusinessLogic()
+        {
+            //if (checkBoxChecked == true)
+            //{
+            //    TargetText = "???";
+            //}
+        }
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- Study Chapter 5/MainWindow.xaml.cs
+++ Study Chapter 5/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
 
 
 namespace Study_Chapter_5
@@ -22,14 +24,138 @@ namespace Study_Chapter_5
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainViewModel mvm = new MainViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
         }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // DataContext에 ViewModel 개체를 assign 한다.
+            this.DataContext = mvm;
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
 
         private void ButtonTarget1_Click(object sender, RoutedEventArgs e)
         {
-                
+            Debug.WriteLine(CheckBoxLegacy.IsChecked);
+
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //TextBoxTarget1.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonTarget2_Click(object sender, RoutedEventArgs e)
+        {
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxTarget2.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonSource1_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxSource1.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonSource2_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxSource2.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonDelete1_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxDelete1.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonDelete2_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxDelete2.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonDelete3_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxDelete3.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonAutoCheck1_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // TextBoxAutoCheck1.Text = ofd.FileName;
+                mvm.ListBoxItems.Add(ofd.FileName);
+            }
+        }
+
+        private void ButtonAutoCheck2_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxAutoCheck2.Text = ofd.FileName;
+            }
+        }
+
+        private void ButtonAutoCheck3_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxAutoCheck3.Text = ofd.FileName;
+            }
+        }
+
+        public void filecopy()
+        {
+            string fileName = "";
+            string sourcePath = "";
+            string targetPath = TextBoxTarget2.Name;
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string targerFile = System.IO.Path.Combine(targetPath, fileName);
+
+            if (!System.IO.Directory.Exists(targetPath))
+            {
+                System.IO.Directory.CreateDirectory(targetPath);
+            }
+            System.IO.File.Copy(sourceFile, targerFile, true);
+
+            if (System.IO.Directory.Exists(sourcePath))
+            {
+                string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                foreach (string s in files)
+                {
+                    fileName = System.IO.Path.GetFileName(s);
+                    targerFile = System.IO.Path.Combine(targetPath, fileName);
+                    System.IO.File.Copy(s, targerFile, true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist!");
+            }
+        }
+
+        private void ButtonCopyStart1_Click(object sender, RoutedEventArgs e)
+        {
+                mvm.CheckBoxChecked = !mvm.CheckBoxChecked;
         }
     }
 }
